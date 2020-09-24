92921919f8965ffb2d1e954e6e69966f2de31dd2
Sergey Firsov <fearthunder@yandex.ru>
2020-09-24 19:26:04 +0300

    Add base item

---
+++ Source/Survival/Structs/Items/ItemData.h
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "ItemData.generated.h"
+
+UENUM(BlueprintType)
+enum class EItemType : uint8
+{
+	Item	UMETA(DisplayName = "Item")
+};
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class SURVIVAL_API UItemData : public UObject
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	EItemType ItemType;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FName ItemName;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float ItemCount;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	bool bIsStackable;
+	
+};
